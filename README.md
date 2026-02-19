59e90c08d7117bb8af4a89278d7a9acf4ae63438
Moritz Moxter <moritz@clockworks.io>
2026-02-19 23:30:25 +0100

    docs: examples and readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Rasterkasten
+
+## Development
+
+The `development` directory provides a Clojure project used run and improve rasterkasten. Repl driven development is encouraged, as such the development project has a babashka task that starts an nrepl server loading all bricks and products of the repo into a single interactive development project. Just connect your favorite repl enabled editor and start building.
--- development/src/development.clj
+++ development/src/development.clj
@@ -5,34 +5,53 @@
    [rasterkasten.render :refer [render*]]))
 
 
-
 (comment
+  ;;
+  ;; ## Examples
+  ;; This provides some examples how to specify and render boxes.
+  ;; Just uncomment the model you would like to render.
+  ;;
+  
   (render* 
-    "build/rendered.openscad"
+    "build/rendered.scad"
     (with-fn 128
     
-    ; (box!
-    ;   {:spec '(
-    ;            [[] | [] _ []]
-    ;            [ _   _    _ ]
-    ;            [[] | [] _ []]
-    ;            [ _   _    _ ]
-    ;            [[] | [] _ []]
-    ;            )
-    ;    :opts {:height :half
-    ;           :fit :loose}})
-    
-    ; (box!
-    ;   {:spec '(
-    ;            [[] _ []]
-    ;            [_    _ ]
-    ;            [[] _ []]
-    ;            [_    _ ]
-    ;            [[] _ []]
-    ;            )
-    ;    :opts {:height :half
-    ;           :fit :loose}})
+      ;; Build a 3x3 half height box with some creative dividers
+      ;; The box uses a loose fitting snap for the grid
+      ; (box!
+      ;   {:spec '(
+      ;            [[] | [] _ []]
+      ;            [ =   _    _ ]
+      ;            [[] | [] _ []]
+      ;            [ _   _    _ ]
+      ;            [[] | [] _ []]
+      ;            )
+      ;    :opts {:height :half
+      ;           :fit :loose}})
       
-
-    (snapgrid 3 3)  
-    )))
+      ;; Build a 2x2 box with full height and snaps fitting tightly into the grid
+      ;; Also add one vertical divider over the full with of the box
+      ; (box!
+      ;   {:spec '(
+      ;            [[] _ []]
+      ;            [=    = ]
+      ;            [[] _ []]
+      ;            )
+      ;    :opts {:height :full
+      ;           :fit :tight}})
+      
+      ;; Build a 2x3 box with no dividers, half height and loose fit
+      ; (box!
+      ;   {:spec '(
+      ;            [[] _ []]
+      ;            [_    _ ]
+      ;            [[] _ []]
+      ;            [_    _ ]
+      ;            [[] _ []]
+      ;            )
+      ;    :opts {:height :half
+      ;           :fit :loose}})
+        
+      ;; Build a 3x3 grid
+      ; (snapgrid 3 3)  
+      )))
